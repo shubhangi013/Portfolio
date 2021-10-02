d2c726c2099c1d775f63fc8c36cdebf748eca73a
shubhangi013 <78342516+shubhangi013@users.noreply.github.com>
2021-10-02 18:32:41 +0530

    updated resume link

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,7 +8,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
           <a href="#intro">About</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
-          <a href="https://drive.google.com/file/d/1HC_Y9aSdLXF6L5Mw0cdPMt8LU2g9CqCc/view?usp=sharing">Resume</a>
+          <a href="https://drive.google.com/file/d/14MUCM86yyDohUOIUt2Rb2vFCATS84wY3/view?usp=sharing">Resume</a>
         </li>
         <li onClick={()=>setMenuOpen(false)}>
           <a href="#works">Projects</a>
